67ce670abcc03df2e0ce3c6d860e34e27bd2c94d
MarkDat <mark.dat.code@gmail.com>
2022-09-24 10:47:49 +0700

    first commit

---
+++ src/app/app-navigation.ts
@@ -0,0 +1,17 @@
+export const navigation = [
+  {
+    text: 'Thống kê',
+    path: '/dashboard',
+    icon: 'fa-solid fa-arrow-trend-up',
+  },
+  {
+    text: 'Danh sách phòng',
+    icon: 'fa-solid fa-table-list',
+    path: ''
+  },
+  {
+    text: 'Cài đặt chung',
+    icon: 'fa fa-cog',
+    path: '',
+  },
+];
